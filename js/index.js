5fadaa3b23e89ba01e095971ff654a9108b38383
mecatronabi <ing.nab@hotmail.com>
2018-04-17 20:10:17 -0500

    Agregué formulario y configuración de base de datos

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('Hola mundo');
+"use strict";
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const city = document.getElementById('city');
+    const message = document.getElementById('message');
+
+    const params = {
+      name: name.value,
+      email: email.value,
+      city: city.value,
+      phone: phone.value,
+      message = message.value
+    }
+    console.log(params)
+
+})
